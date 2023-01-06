d15dac03eb85ed5441976c247d2f18cd736495cb
Ana Solcan <solcan.anna@gmail.com>
2023-01-06 15:03:41 +0200

    added grid, some css styling, js toggle between classes on click

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const boxes = document.querySelectorAll(".box");
+let color = "pink";
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    switch (color) {
+      case "pink":
+        box.classList.toggle("box-x", true);
+        color = "blue";
+        break;
+      case "blue":
+        box.classList.toggle("box-o", true);
+        color = "pink";
+        break;
+    }
+  });
+});
 
+//   box.removeEventListener("click", handleClick);
