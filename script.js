d1ab5ab85641c852aad6ee4ab54bc368ff1f17f0
Ana Solcan <solcan.anna@gmail.com>
2023-01-09 16:12:40 +0200

    got the checkWin function to work, removed handleclick function and integrated it elsewhere

--- script.js
+++ script.js
@@ -1,55 +1,111 @@
-let color = "pink";
 let resetButton = document.querySelector(".reset");
 let playerTurn = document.querySelector(".playerTurn");
-
 const boxes = document.querySelectorAll(".box");
+let color = "pink";
+let playerX = [];
+let playerO = [];
 
-resetButton.addEventListener("click", reset);
+// state an array of winning combinations
+const winCombinations = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
 
-boxes.forEach((box) => {
-  box.addEventListener("click", handleBoxClicked);
-});
+// boxes.forEach((box) => {
+//   box.addEventListener("click", handleBoxClicked);
+// });
 
-boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    if (box.getAttribute("data-clicked") === "true") return;
-    switch (color) {
-      case "pink":
-        // box.innerHTML = "X";
-        box.classList.add("box-x");
-        color = "blue";
-        playerTurn.innerHTML = "Player <span class='x'>O's</span> Turn 🚀";
-        break;
-      case "blue":
-        // box.innerHTML = "O";
-        box.classList.add("box-o");
-        color = "pink";
-        playerTurn.innerHTML = "Player <span class='x'>X's</span> Turn 🚀";
-        break;
-    }
-    box.setAttribute("data-clicked", "true");
+// start the game
+function startGame() {
+  color = "pink";
+  playerTurn.innerHTML = "Player <span class='item-x'>X's</span> Turn 🚀";
+
+  //   add event listener to the box clicked
+  boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+      if (box.getAttribute("data-clicked") === "true") return;
+
+      switch (color) {
+        case "pink":
+          box.classList.add("box-x");
+          color = "blue";
+          playerTurn.innerHTML =
+            "Player <span class='item-o'>O's</span> Turn 🚀";
+          playerX.push(parseInt(box.id));
+          break;
+        case "blue":
+          box.classList.add("box-o");
+          color = "pink";
+          playerTurn.innerHTML =
+            "Player <span class='item-x'>X's</span> Turn 🚀";
+          playerO.push(parseInt(box.id));
+          break;
+      }
+      console.log(playerX);
+      console.log(playerO);
+      box.setAttribute("data-clicked", "true");
+      checkwin();
+    });
   });
-});
-
-// handle Box Clicked function
-function handleBoxClicked(e) {
-  let box = +e.target.getAttribute("id");
-  e.target.removeEventListener("click", handleBoxClicked);
-  console.log(box);
 }
 
-// handle Reset Game
+// add event listener to reset button
+resetButton.addEventListener("click", reset);
+
+// handle Box clicked function
+// function handleBoxClicked(e) {
+//   let box = +e.target.getAttribute("id");
+//   if (color === "pink") {
+//     color = "blue";
+//     playerX.push(box);
+//   } else {
+//     playerO.push(box);
+//     color = "pink";
+//   }
+//   console.log(playerX);
+//   console.log(playerO);
+//   e.target.removeEventListener("click", handleBoxClicked);
+// }
+
+// handle win / lose / tie
+function checkwin() {
+  let playerXWon = winCombinations.some((combination) => {
+    return combination.every((number) => {
+      return playerX.includes(number);
+    });
+  });
+  let playerOWon = winCombinations.some((combination) => {
+    return combination.every((number) => {
+      return playerO.includes(number);
+    });
+  });
+  if (playerXWon) {
+    console.log("Player X won!");
+  } else if (playerOWon) {
+    console.log("Player O won!");
+  } else if (playerX.length + playerO.length === 9) {
+    console.log("It's a tie!");
+  }
+}
+
+// handle Reset button
 function reset() {
   boxes.forEach((box) => {
     box.classList.remove("box-x", "box-o");
     box.setAttribute("data-clicked", "false");
+    playerX = [];
+    playerO = [];
     console.clear();
-    box.addEventListener("click", handleBoxClicked);
-    playerTurn.innerHTML = "Player <span class='x'>X's</span> Turn 🚀";
   });
+  startGame();
 }
 
-// handle Draw
-// handle win or lose
+startGame();
+
 // handle who's turn it is --> display message Player X's or Player O's turn; or Who wins; Tie;
-// change box design switch from color to X or O
